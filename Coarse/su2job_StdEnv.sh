43bd6788122245d72f06f5c46d8e0bce45bacad7
j6joseph <159580586+j6joseph@users.noreply.github.com>
2024-03-05 19:14:06 -0500

    Update su2job_StdEnv.sh

--- Coarse/su2job_StdEnv.sh
+++ Coarse/su2job_StdEnv.sh
@@ -6,5 +6,5 @@
 #SBATCH --output=BFSstr%j.txt
 #SBATCH --mail-type=FAIL
 cd $SLURM_SUBMIT_DIR
-module load CCEnv StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 su2/7.5.1
+module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 su2/7.5.1
 mpirun -n 10 SU2_CFD Backstep_str_config.cfg
